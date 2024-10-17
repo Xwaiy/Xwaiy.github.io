94e86e160d654667c959a0ccd9e857f059118993
Xwaiy <2312368371@qq.com>
2024-10-17 17:47:16 +0800

    Site updated: 2024-10-17 17:47:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/16/hello-world/"];function toRandomPost(){
+var posts=["2024/10/17/每日十问前端面试题Vue篇-02/","2024/10/16/每日十问前端面试题Vue篇-01/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
