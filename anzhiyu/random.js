d033393e5717bbc68726d2c149a03f479c710105
Xwaiy <2312368371@qq.com>
2024-10-18 10:58:44 +0800

    Site updated: 2024-10-18 10:58:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/17/每日十问前端面试题Vue篇-02/","2024/10/16/每日十问前端面试题Vue篇-01/"];function toRandomPost(){
+var posts=["2024/10/18/每日十问前端面试题Java篇-02/","2024/10/18/每日十问前端面试题Java篇-01/","2024/10/16/每日十问前端面试题Vue篇-01/","2024/10/17/每日十问前端面试题Vue篇-02/","2024/10/18/每日十问前端面试题Vue篇-03/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
